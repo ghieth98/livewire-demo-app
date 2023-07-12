7505ddd75befe8db1c88f4356ccd47bcc9c4eefe
hossam ghieth <hossamghieth@gmail.com>
2023-07-12 15:27:01 +0300

    Added product and search functionality
    Product can be searched using :
    name
    categories
    country
    price

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+        Product::create([
+            'name' => fake()->colorName,
+            'description' => fake()->text(20),
+            'country_id' => rand(1, 20),
+            'price' => rand(1000, 10000)
+        ]);
+
+    }
+}
